11babfa7356bc9bc2395cc4a9102d6c426816b93
kellypaulinc <kelly.narvaez1@gmail.com>
2022-03-04 12:07:40 -0500

    Dummy

--- src/main/java/org/jlgranda/appsventas/api/controller/app/SRIComprobantesController.java
+++ src/main/java/org/jlgranda/appsventas/api/controller/app/SRIComprobantesController.java
@@ -342,6 +342,7 @@ public class SRIComprobantesController {
                 }
             }
         }
+
         return Api.responseError("El servidor no pudo generar el comprobante", new Error());
     }
 
