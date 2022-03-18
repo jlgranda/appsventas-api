391b6f8c877027f8ae7a528b98e4736595d128ba
José Luis Granda <jlgranda81@gmail.com>
2022-03-18 11:51:17 -0500

    Soporte para guardar foto

--- src/main/java/org/jlgranda/appsventas/domain/app/Organization.java
+++ src/main/java/org/jlgranda/appsventas/domain/app/Organization.java
@@ -16,10 +16,8 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.Table;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.jlgranda.appsventas.domain.DeletableObject;
 
 /**
